4975cce707abf627d45da78fc52cdc9ce742f6d4
fana <fanabezabih@gmail.com>
2025-06-03 06:47:12 +0300

    updated style.css for images

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-//Question1
+
+// Question 1: Change document background color to silver
 document.body.style.backgroundColor = "silver";
 
-//Question 2
-const color= document.getElementById('title')
-color.style.color='green'
+// Question 2: Change font color for h1 title to green
+const color = document.getElementById("title");
+color.style.color = "green";
 
-//Question 3
-const element = document.getElementById('heading');
-const originalText = element.textContent;const uppercaseText = originalText.toUpperCase();
-element.textContent = uppercaseText;
+// Question 3: Change font case for h3 tags to uppercase
 
-//Question4
+document.querySelectorAll("h3").forEach(element => {
+  element.classList.add("uppercase"); 
+});
+
+// Question 4: Add one more fruit to the fruits list
 const newFruit = document.createElement("li");
 newFruit.textContent = "Apples";
 document.getElementById("fruList").appendChild(newFruit);
 
-//Question5
+// Question 5: Add one more vegetable to the vegetables list
 const newVeg = document.createElement("li");
 newVeg.textContent = "Carrots";
 document.getElementById("vegList").appendChild(newVeg);
